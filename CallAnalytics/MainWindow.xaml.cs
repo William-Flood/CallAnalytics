41bdcde26e0a890150e9e2c2562ee83df8e339aa
Bill Flood <wijaflood@gmail.com>
2018-08-05 16:49:40 -0500

    Adding file select and settings manager

--- CallAnalytics/MainWindow.xaml.cs
+++ CallAnalytics/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Control;
+using System.IO;
+using Path = System.IO.Path;
 
 namespace CallAnalytics
 {
@@ -20,9 +24,22 @@ namespace CallAnalytics
     /// </summary>
     public partial class MainWindow : Window
     {
+        SettingsManager _settingsManager;
         public MainWindow()
         {
             InitializeComponent();
+            _settingsManager = new SettingsManager();
+        }
+
+        private void LoadFiles(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog
+            {
+                InitialDirectory = _settingsManager.LastSearchedFolder
+            };
+            fileDialog.ShowDialog();
+            _settingsManager.LastSearchedFolder = Path.GetDirectoryName(fileDialog.FileName);
+
         }
     }
 }
---
+++ Control/SettingsManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace Control
+{
+    public class SettingsManager
+    {
+        const string FOLDERNAME = "CallAnalytics";
+        const string FILENAME = "Config.xml";
+        const string LAST_SEARCHED_FOLDER_ELEM = "lastSearchedFolder";
+        public SettingsManager()
+        {
+            var settingsFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), FOLDERNAME);
+            if(!Directory.Exists(settingsFolder))
+            {
+                Directory.CreateDirectory(settingsFolder);
+            }
+            var settingsFile = Path.Combine(settingsFolder, FILENAME);
+            if(File.Exists(settingsFile))
+            {
+                try
+                {
+                    using(var netReader = XmlReader.Create(settingsFile))
+                    {
+                        var netDocument = new XmlDocument();
+                        netDocument.Load(netReader);
+                        var root = netDocument.LastChild;
+                        _lastSearchedFolder = root.SelectNodes(LAST_SEARCHED_FOLDER_ELEM)[0].InnerText;
+                    }
+                } catch(DirectoryNotFoundException ex)
+                {
+                    throw new Exception("Couldn't load file", ex);
+                }
+            }
+        }
+
+        private String _lastSearchedFolder;
+        public String LastSearchedFolder
+        {
+            get
+            {
+                return _lastSearchedFolder;
+            }
+            set
+            {
+                _lastSearchedFolder = value;
+                Save();
+            }
+        }
+
+        public void Save()
+        {
+            var saveFile = Path.Combine(new String[] { Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), FOLDERNAME, FILENAME});
+            try
+            {
+                using(var netWriter = XmlWriter.Create(saveFile))
+                {
+                    netWriter.WriteStartElement("setting");
+                    netWriter.WriteElementString(LAST_SEARCHED_FOLDER_ELEM, _lastSearchedFolder);
+                    netWriter.WriteEndElement();
+                    netWriter.Flush();
+                }
+            } catch (Exception ex)
+            {
+                throw new Exception("Error saving", ex);
+            }
+        }
+    }
+}
